fa7b22ddcb327337d141f0e5b6995f3e89828095
JayWilson <heyjay@cctplus.dev>
2022-11-26 20:15:19 -0600

    build: adds initial danger file
    Has a pursposeful failure

---
+++ dangerfile.swift
@@ -0,0 +1,27 @@
+import Danger
+let danger = Danger()
+
+// MARK: Constants
+let hasChangelog = danger.git.modifiedFiles.contains("changelog.md")
+let bodyText = danger.github.pullRequest.body ?? ""
+let titleText = danger.github.pullRequest.title
+let isTrivial = (bodyText + titleText).contains("#trivial")
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+let bigPRThreshold: Int = 600;
+let additions: Int = danger.github.pullRequest.additions ?? 0
+let deletions: Int = danger.github.pullRequest.deletions ?? 0
+
+// MARK: Changelog
+
+if (!hasChangelog && !isTrivial) {
+    warn("Please add a changelog entry for your changes.")
+}
+
+// MARK: PR Count
+
+if (additions > bigPRThreshold) {
+  warn("> Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
+}
+if (additions < bigPRThreshold) {
+  fail("PR IS TOO SMALL")
+}
